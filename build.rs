dc13aee06258bb6b5008dd06c046ab6703395a05
meijie <meijie@laiye.com>
2022-11-28 23:06:50 +0800

    init project

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/model-mesh.proto")?;
+    tonic_build::configure()
+        .build_client(false)
+        .compile(&["proto/model-mesh.proto"], &["proto/"])?;
+    tonic_build::configure()
+        .build_server(false)
+        .compile(&["proto/model-runtime.proto"], &["proto/"])?;
     Ok(())
 }
